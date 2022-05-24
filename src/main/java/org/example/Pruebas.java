e43ee3cf6ad1c041ef12355d734ac11e9f857672
BungaEdu <eduardoflores.9595@gmail.com>
2022-05-24 22:11:14 +0200

    prueba2

--- src/main/java/org/example/Pruebas.java
+++ src/main/java/org/example/Pruebas.java
@@ -1,9 +1,21 @@
 package org.example;
 
+import com.sun.jdi.StringReference;
+
+import java.util.Scanner;
+
 public class Pruebas {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int num = 123;
+        String numToString = String.valueOf(num);
+        String texto = "perro";
+        try {
+            int resultado= numToString-3;
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
 }
 
